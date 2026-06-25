ffbd77a2811317416d75a0c0b9f84c3d21682aca
ziomek3120 <unboxorg@gmail.com>
2026-06-25 17:09:29 +0200

    sweet: Infinity bringup

--- infinity_sweet.mk
+++ infinity_sweet.mk
@@ -12,10 +12,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Infinity stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+## Infinity flags
+
+# Maintainer Name
+INFINITY_MAINTAINER := "unboxorg"
+
+# Whether Including Google Apps
+WITH_GAPPS := true
+
+PRODUCT_NAME := infinity_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
